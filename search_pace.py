d39cd169f96ac6c0ea65cf4b34b2f4b7bad6d5b8
oleg <314koval.ok@gmail.com>
2021-01-27 19:18:12 +0200

    логика работает

--- search_pace.py
+++ search_pace.py
@@ -27,18 +27,19 @@ def do_search() ->'html':
     sc = request.form['sc'] # секунды
     s = request.form['s'] # дистанция
     finish = datetime.timedelta(hours = int(hr), minutes = int(mt), seconds = int(sc)) # время завершения дистанции     
-    vdot = str(find_vdot(finish, dist[s]))
-    the_e = pace[vdot]['e'] # pace easy
-    the_m = pace[vdot]['m'] # pace marathon
-    the_t400 = pace[vdot]['t400'] # pace threshold 400m
-    the_t1000 = pace[vdot]['t1000'] # pace threshold 1000m
-    the_i400 = pace[vdot]['i400'] # pace interval 400m
-    the_i1000 = pace[vdot]['i1000'] # pace interval 1000m
-    the_i1200 = pace[vdot]['i1200'] # pace interval 12000m
-    the_r200 = pace[vdot]['r200'] # pace repetition 200m
-    the_r400 = pace[vdot]['r400'] # pace repetition 400m 
-    the_r800 = pace[vdot]['r800'] # pace repetition 800m    
-    return render_template('pace.html', the_e, the_m, the_t400,
-                           the_t1000, the_i400, the_i1000, the_i1200,
-                           the_r200, the_r400, the_r800, the_title=title)
+    vdot = str(find_vdot(finish, dist[s]))   
+    e = pace[vdot]['e'] # pace easy    
+    m = pace[vdot]['m'] # pace marathon
+    t400 = pace[vdot]['t400'] # pace threshold 400m
+    t1000 = pace[vdot]['t1000'] # pace threshold 1000m
+    i400 = pace[vdot]['i400'] # pace interval 400m
+    i1000 = pace[vdot]['i1000'] # pace interval 1000m
+    i1200 = pace[vdot]['i1200'] # pace interval 12000m
+    r200 = pace[vdot]['r200'] # pace repetition 200m
+    r400 = pace[vdot]['r400'] # pace repetition 400m 
+    r800 = pace[vdot]['r800'] # pace repetition 800m 
+    
+    return render_template('pace.html', the_vdot=vdot, the_e=e, the_m=m, the_t400=t400,
+                           the_t1000=t1000, the_i400=i400, the_i1000=i1000, the_i1200=i1200,
+                           the_r200=r200, the_r400=r400, the_r800=r800, the_title=title)       
 app.run()
