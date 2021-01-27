5b71fe2b319c8f1a9d2bde0ff42d0224c6b02833
olekoval <31569953+olekoval@users.noreply.github.com>
2021-01-27 12:13:07 +0200

    Update search_pace.py

--- search_pace.py
+++ search_pace.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 import datetime
 from vdot import find_vdot # импотр функции поиска VDOT
-from list_vdot import km_5, km_10 # импорт списков времени прохождения дистанции в зависимости от VDOT
-
+from list_vdot import dist, km_5, km_10 # импорт списков времени прохождения дистанции
+                                        # в зависимости от VDOT
 
 app = Flask(__name__)
 
@@ -25,10 +25,10 @@ def do_search() ->'html':
     mt = request.form['mt'] # минуты
     sc = request.form['sc'] # секунды
     s = request.form['s'] # дистанция
-    #finish = datetime.timedelta(hours = 0, minutes = 62, seconds = 1) # данные от пользователя
-    #s = '10km' # 10km - получить от пользователя
-    dist = {'5km':km_5, '10km':km_10}
-    #return find_vdot(finish, dist[s]) # list_vdot.km_10 список времен финиширования
-    return render_template('results.html', the_hr=hr, the_mt=mt, the_sc=sc, the_s=s, the_title=title)
+    finish = datetime.timedelta(hours = int(hr), minutes = int(mt), seconds = int(sc)) # время завершения дистанции     
+    vdot = find_vdot(finish, dist[s])
+    
+    
+    #return render_template('results.html', the_hr=hr, the_mt=mt, the_sc=sc, the_s=s, the_title=title)
 
 app.run()
